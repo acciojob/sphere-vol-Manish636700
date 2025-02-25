ebad5d3c4b796c432d54681f5453dbe6efe9faeb
Manish Kumar Mittal <98324095+Manish636700@users.noreply.github.com>
2025-02-25 20:08:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function volume_sphere() {
     //Write your code here
-  
+ event.preventDefault();
+
+	let redius = document.getElementById("radius").value;
+	let r = parseFloat(redius);
+
+	if(isNaN(r) || r<0){
+		document.getElementById("volume").value="NaN";
+		return;
+	}
+
+	let v = (4/3)*Math.PI*Math.pow(r,3);
+
+	document.getElementById("volume").value = v.toFixed(4);
+	
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
